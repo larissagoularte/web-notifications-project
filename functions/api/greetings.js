c3607fb36afedac1d1bd55491e5553b1488676a6
Larissa Goularte <58703854+larissagoularte@users.noreply.github.com>
2024-07-14 11:22:45 +0100

    functions

---
+++ functions/api/greetings.js
@@ -0,0 +1,8 @@
+export const onRequestGet = () => {
+    return new Response("Hello world")
+}
+
+export const onRequestPost = async ({ request }) => {
+    const { name } = await request.json();
+    return new Response(`Hello ${name}`)
+}
