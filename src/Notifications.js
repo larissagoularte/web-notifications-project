ee4c0e96bb0c92c5c808129e6f95ff00bac6a45a
Larissa Goularte <58703854+larissagoularte@users.noreply.github.com>
2024-07-14 13:05:28 +0100

    get notifications - styles config

--- src/Notifications.js
+++ src/Notifications.js
@@ -83,7 +83,7 @@ const Notifications = () => {
             <div className="feed-container md:w-1/2 bg-white md:py-10 md:pr-10">
                 <div id="notification-feed" className='bg-neutral-100 rounded-lg p-5 h-[400px]'>
                     {notifications.map((notification) => (
-                        <div id='notification-card' key={notification.id} className='flex flex-col p-4 gap-[10px]' style={{ backgroundColor: getBackgroundColor(notification.type) }}>
+                        <div id='notification-card' key={notification.id} className='h-[70px] flex flex-col p-4 gap-[10px]' style={{ backgroundColor: getBackgroundColor(notification.type) }}>
                             <div>{notification.content.text}</div>
 
                             <div className='opacity-80'>
